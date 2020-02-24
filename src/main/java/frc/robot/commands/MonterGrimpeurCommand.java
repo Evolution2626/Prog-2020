5692005052b260d77febedbf352e7a522ec2525e
Zach3292 <Zach3292@hotmail.com>
2020-02-24 14:42:14 -0500

    add MonteurGrimpeurCommand

--- src/main/java/frc/robot/commands/MonterGrimpeurCommand.java
+++ src/main/java/frc/robot/commands/MonterGrimpeurCommand.java
@@ -8,19 +8,17 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.Feeder;
-import frc.util.records.Logs;
+import frc.robot.subsystems.Grimpeur;
 
-public class MonterPourTirerCommand extends CommandBase {
+public class MonterGrimpeurCommand extends CommandBase {
   /**
-   * Creates a new MonterPourTirer.
+   * Creates a new MonterGrimpeurCommand.
    */
-  private Feeder feeder;
-
-  public MonterPourTirerCommand(Feeder feeder) {
+  private Grimpeur grimpeur;
+  public MonterGrimpeurCommand(Grimpeur grimpeur) {
+    this.grimpeur = grimpeur;
+    addRequirements(grimpeur);
     // Use addRequirements() here to declare subsystem dependencies.
-    this.feeder = feeder;
-    addRequirements(feeder);
   }
 
   // Called when the command is initially scheduled.
@@ -31,20 +29,18 @@ public class MonterPourTirerCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    feeder.enableFeeder(true);
+    grimpeur.setMonteurSpeed(1);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    feeder.enableFeeder(false);
-    Logs.addEvenement("Ballon Monté");
-
+    grimpeur.setMonteurSpeed(0);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return feeder.getCapteurValue(4);
+    return grimpeur.getLimitSwitchValue();
   }
 }
