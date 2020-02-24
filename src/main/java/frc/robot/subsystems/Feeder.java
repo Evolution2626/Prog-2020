9dcbd8ee88837a02ab1f040f3f0680d451e8a8c6
Zach3292 <Zach3292@hotmail.com>
2020-02-24 14:43:49 -0500

    update the number of sensor

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -8,10 +8,10 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -19,69 +19,44 @@ public class Feeder extends SubsystemBase {
   /**
    * Creates a new Feeder.
    */
-  private VictorSPX feeder;
-  private DigitalInput[] capteurs = new DigitalInput[5];
-  private DigitalInput capteurRest;
-  public DigitalInput capteurLanceur;
+  private VictorSPX feederHaut;
+  private VictorSPX feederBas;
 
+  private DigitalInput[] capteurs = new DigitalInput[3];
 
 
   public Feeder() {
-    feeder = new VictorSPX(Constants.CAN.MOTEUR_FEEDER);
+    feederHaut = new VictorSPX(Constants.CAN.MOTEUR_FEEDER_HAUT);
+    feederBas = new VictorSPX(Constants.CAN.MOTEUR_FEEDER_BAS);
 
-    capteurs[0] = new DigitalInput(Constants.DIO.CAPTEUR_UN);
-    capteurs[1] = new DigitalInput(Constants.DIO.CAPTEUR_DEUX);
-    capteurs[2] = new DigitalInput(Constants.DIO.CAPTEUR_TROIS);
-    capteurs[3] = new DigitalInput(Constants.DIO.CAPTEUR_QUATRE);
-    capteurs[4] = new DigitalInput(Constants.DIO.CAPTEUR_CINQ);
-    capteurRest = new DigitalInput(Constants.DIO.CAPTEUR_REST);
-    capteurLanceur = new DigitalInput(Constants.DIO.CAPTEUR_LANCEUR);
+    capteurs[0] = new DigitalInput(Constants.DIO.CAPTEUR_DEUX);
+    capteurs[1] = new DigitalInput(Constants.DIO.CAPTEUR_TROIS);
+    capteurs[2] = new DigitalInput(Constants.DIO.CAPTEUR_QUATRE);
+    
+    feederBas.setNeutralMode(NeutralMode.Brake);
+    feederHaut.setNeutralMode(NeutralMode.Brake);
+    
 
   }
 
-  public void setSpeed(double speed){
-    feeder.set(ControlMode.PercentOutput, speed);
+  public void setSpeedHaut(double speed) {
+    feederHaut.set(ControlMode.PercentOutput, speed);
   }
 
-  public void enableFeeder(boolean enable){
-    if (enable) {
-      setSpeed(Constants.SPEED.FEEDER_SPEED);
-    }else {
-      setSpeed(0);
-    }
+  public void setSpeedBas(double speed) {
+    feederBas.set(ControlMode.PercentOutput, speed);
   }
 
-  public boolean getCapteurValue(int capteur){
-    return !capteurs[capteur].get();
+  public boolean getCapteurValue(int capteur) {
+    return capteurs[capteur].get();
   }
 
-  public boolean getCapteurRestValue(){
-    return !capteurRest.get();
+  public DigitalInput getCapteurRaw(int capteur) {
+    return capteurs[capteur];
   }
-
-  public DigitalInput getCapteurRest(){
-    return capteurRest;
-  }
-
-  public boolean getCapteurLanceurValue(){
-    return !capteurLanceur.get();
-  }
-
-  public int countBallon(){
-    int count = 0;
-    for(int i = 0; i <= 4; i++){
-      if (getCapteurValue(i)) {
-        count ++;
-      }
-    }
-    return count;
-  }
-
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    for (int i = 0; i <5; i++) {
-      SmartDashboard.putBoolean("Capteur" + i , getCapteurValue(i));
-    }
   }
 }
