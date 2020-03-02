d8ee96490b6e7161d2af3857a684c94e08601436
Zach3292 <Zach3292@hotmail.com>
2020-03-02 13:09:34 -0500

    remove rumble

--- src/main/java/frc/robot/commands/DrivetrainDriveCommand.java
+++ src/main/java/frc/robot/commands/DrivetrainDriveCommand.java
@@ -12,7 +12,6 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
-import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.Drivetrain;
